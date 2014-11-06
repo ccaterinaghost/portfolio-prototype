c14cea551c0814d3f33dae8770f406b55537c43f
ccaterinaghost <ccat.x@Hotmail.com>
2014-11-06 09:49:12 -0500

    links js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var $win = $('window');
 var $logo = $('.logo');
-var $about = $('.about')
+var $about = $('.about');
 var $links = $('.links');
 var $portfolio = $('.portfolio')
 
 $about.waypoint(function (direction) {
 	if (direction == 'down') {
 		$logo.addClass('js-logo-move');
+		$links.addClass('js-links-move');
 	} else {
 		$logo.removeClass('js-logo-move');
+		$links.removeClass('js-links-move');
 	}
 }, {offset: '70%'});
-
-
